000ba8453de10b0ca2658f8ba1fd0335ab563f42
tbm5k <tedburg5@gmail.com>
2022-03-22 19:47:56 +0300

    imported maps api and created a map component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import InfoBar from "./components/InfoBar";
+import Map from "./components/Map";
 
 function App() {
 
@@ -9,7 +10,12 @@ function App() {
 
   return (
     <div className="App" style={componentStyle}>
-      <InfoBar />
+      <Map
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=&v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     </div>
   );
 }
