ffba9156b5e74ea6852070cd52c5d05edb0787db
tbm5k <tedburg5@gmail.com>
2022-03-24 09:06:44 +0300

    position the info bar

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,6 +5,8 @@ import {
   Marker,
 } from "react-google-maps";
 
+import InfoBar from './InfoBar'
+
 let latitude = -1.278286791538775
 let longitude = 36.81009696210602
 
@@ -16,6 +18,8 @@ const Map = withScriptjs(withGoogleMap(props =>
     <Marker
       position={{ lat: latitude, lng: longitude }}
     />
+
+    <InfoBar />
   </GoogleMap>
 ));
 export default Map
