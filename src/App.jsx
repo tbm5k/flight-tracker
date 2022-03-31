cd5e7bb00bff5c47c65c41008652ba3f627868a5
tbm5k <tedburg5@gmail.com>
2022-03-31 10:37:05 +0300

    changed the map api to one whose overlay component actually works

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import InfoBar from "./components/InfoBar";
 import Map from "./components/Map";
 
 function App() {
@@ -10,12 +9,7 @@ function App() {
 
   return (
     <div className="App" style={componentStyle}>
-      <Map
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAPS_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100%` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
+      <Map/>
     </div>
   );
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,25 +1,54 @@
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-} from "react-google-maps";
+import { GoogleMap, LoadScript, OverlayView } from '@react-google-maps/api';
 
-import InfoBar from './InfoBar'
+const Map = props => {
 
-let latitude = -1.278286791538775
-let longitude = 36.81009696210602
+  const containerStyle = {
+    width: '100%',
+    height: '100%'
+  };
+  
+  const center = {
+    lat: -1.278286791538775,
+    lng: 36.81009696210602
+  };
+  
+  const onClick = () => {
+    console.info('I have been clicked!')
+  };
+  
+  const divStyle = {
+    background: 'white',
+    border: '1px solid #ccc',
+    padding: 15
+  };
+    
+  return(
+    <LoadScript
+        googleMapsApiKey={`${process.env.REACT_APP_MAPS_API_KEY}`}
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+        >
+          <OverlayView
+            position={center}
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+          >
+            <div style={divStyle}>
+              <h1>OverlayView</h1>
+              <button
+                onClick={onClick}
+                type='button'
+              >
+                Click me
+              </button>
+            </div>
+          </OverlayView>
+        </GoogleMap>
+      </LoadScript>
+  )
+}
+  
 
-const Map = withScriptjs(withGoogleMap(props =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: latitude, lng: longitude }}
-  >
-    <Marker
-      position={{ lat: latitude, lng: longitude }}
-    />
-
-    <InfoBar />
-  </GoogleMap>
-));
 export default Map
