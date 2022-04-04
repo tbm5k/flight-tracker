a671099eac032ef290e0f96a38b78f5f86a4454e
tbm5k <tedburg5@gmail.com>
2022-04-04 22:34:14 +0300

    rendering flight data on click

--- src/components/InfoBar.jsx
+++ src/components/InfoBar.jsx
@@ -1,26 +1,26 @@
 import './InfoBar.css'
 
-const InfoBar = () => {
+const InfoBar = flight => {
 
     return(
         <div id="info-bar">
             <div>
                 <p className='space-below'>Kenya Airways</p>
-                <p className='space-below'>KQ402</p>
+                <p className='space-below'>{flight.reg_number}</p>
                 <p>E90</p>
             </div>
             <div>
                 <div className='list'>
                     <p className='space-below'>Speed</p>
-                    <p className='space-below'>300kts</p>
+                    <p className='space-below'>{flight.speed}</p>
                 </div>
                 <div className='list'>
-                    <p className='space-below'>Alt</p>
-                    <p className='space-below'>34.345654</p>
+                    <p className='space-below'>Lat</p>
+                    <p className='space-below'>{flight.lat}</p>
                 </div>
                 <div className='list'>
                     <p className='space-below'>Long</p>
-                    <p>34.345654</p>
+                    <p>{flight.lng}</p>
                 </div>
             </div>
             <div>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from 'react';
 const Map = () => {
 
   const [planes, setPlanes] = useState([])
+  const [plane, setPlane] = useState({})
 
   const containerStyle = {
     width: '100%',
@@ -18,12 +19,17 @@ const Map = () => {
   }, [])
 
   const fetchData = async () => {
-    await fetch(`https://airlabs.co/api/v9/flights?_view=array&_fields=hex,flag,lat,lng,dir,alt&api_key=${process.env.REACT_APP_AIRLABS_KEY}`)
+    await fetch(`https://airlabs.co/api/v9/flights?api_key=${process.env.REACT_APP_AIRLABS_KEY}`)
     .then( res => res.json())
     .then( data => {
-      setPlanes(data.slice(300, 400))
+      setPlanes(data.response.slice(300, 400))
     })
     .catch(err => console.log(err))
+  }
+
+  const handleClick = flightData => {
+    setPlane(flightData)
+    console.log(flightData)
   }
 
   return(
@@ -35,20 +41,51 @@ const Map = () => {
           center={{lat: 46.95168156778651, lng: 7.689743550062828}}
           zoom={6}
         >
-          {console.log(planes)}
           {
             planes !== undefined ? (
               planes.map( plane => (
                 <OverlayView 
-                  key={plane[0]}
-                  position={{lat: plane[2], lng: plane[3]}}
+                  key={plane.hex}
+                  position={{lat: plane.lat, lng: plane.lng}}
                   mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
                 >
-                  <FontAwesomeIcon icon={faPlane} style={{width: '30px', height:'30px', color: 'purple'}}/> 
+                  <FontAwesomeIcon icon={faPlane} style={{width: '30px', height:'30px', color: 'purple'}} onClick={() => handleClick(plane)}/> 
                 </OverlayView>
             ))) : null
           }
-          <InfoBar />
+          {/* <InfoBar flight={plane}/> */}
+          <div id="info-bar">
+            <div>
+                <p className='space-below'>Kenya Airways</p>
+                <p className='space-below'>{plane.reg_number}</p>
+                <p>E90</p>
+            </div>
+            <div>
+                <div className='list'>
+                    <p className='space-below'>Speed</p>
+                    <p className='space-below'>{plane.speed}</p>
+                </div>
+                <div className='list'>
+                    <p className='space-below'>Lat</p>
+                    <p className='space-below'>{plane.lat}</p>
+                </div>
+                <div className='list'>
+                    <p className='space-below'>Long</p>
+                    <p>{plane.lng}</p>
+                </div>
+            </div>
+            <div>
+                <p className='space-below'>Nairobi-Lagos</p>
+                <div className='list'>
+                    <p className='space-below'>Departure</p>
+                    <p className='space-below'>12:00</p>
+                </div>
+                <div className='list'>
+                    <p className='space-below'>Arrival</p>
+                    <p>12:00</p>
+                </div>
+            </div>
+        </div>
         </GoogleMap>
       </LoadScript>
   )
