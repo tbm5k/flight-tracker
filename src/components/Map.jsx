f3fcef406c89ac2ba8002fd200fd0939da32508c
tbm5k <tedburg5@gmail.com>
2022-04-04 14:32:23 +0300

    .

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,34 +2,51 @@ import { GoogleMap, LoadScript, OverlayView } from '@react-google-maps/api';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlane } from '@fortawesome/free-solid-svg-icons'
 import InfoBar from './InfoBar'
+import { useEffect, useState } from 'react';
 
 const Map = () => {
+
+  const [planes, setPlanes] = useState([])
 
   const containerStyle = {
     width: '100%',
     height: '100%'
   };
   
-  const center = {
-    lat: -1.278286791538775,
-    lng: 36.81009696210602
-  };
-    
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    await fetch(`https://airlabs.co/api/v9/flights?_view=array&_fields=hex,flag,lat,lng,dir,alt&api_key=${process.env.REACT_APP_AIRLABS_KEY}`)
+    .then( res => res.json())
+    .then( data => {
+      setPlanes(data)
+    })
+    .catch(err => console.log(err))
+  }
+
   return(
     <LoadScript
         googleMapsApiKey={`${process.env.REACT_APP_MAPS_API_KEY}`}
       >
         <GoogleMap
           mapContainerStyle={containerStyle}
-          center={center}
+          center={{lat: 50.3, lng: 3.64}}
           zoom={10}
         >
-          <OverlayView
-            position={center}
-            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
-          >
-            <FontAwesomeIcon icon={faPlane} style={{width: '40px', height:'40px', color: 'purple'}}/>
-          </OverlayView>
+          {
+            planes !== undefined ? (
+              planes.map( plane => (
+                <OverlayView 
+                  key={plane[0]}
+                  center={{lat: plane[2], lng: plane[3]}}
+                  mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+                >
+                  <FontAwesomeIcon icon={faPlane} style={{width: '40px', height:'40px', color: 'purple'}}/> 
+                </OverlayView>
+            ))) : null
+          }
           <InfoBar />
         </GoogleMap>
       </LoadScript>
